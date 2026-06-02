3911992d313d5233f49aa6be6ad2696ef7430755
jeeyyxs <ajengrahmadhani02@gmail.com>
2026-06-02 16:16:45 +0700

    menunjuk kesebuah file  dgn ekstenti txt

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -42,6 +42,12 @@ int main()
     // membuka file dalam mode membaca
     ifstream infile;
 
+    // menunjuk ke sebuah file dengan ekstensi .txt
+    infile.open(NamaFile + ".txt", ios::in);
+
+    cout << endl << ">= Membuka dan membaca file" << endl;
+
+
 
 
 }
